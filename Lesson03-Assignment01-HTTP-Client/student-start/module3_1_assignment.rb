f58a299df402605eb9f94a08f4d7babc5e58e577
kelomaniack <kelomaniack@hotmail.com>
2017-04-20 22:00:13 +0200

    Finished module3_1_assignment

--- Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,17 +1,19 @@
 require 'httparty'
+require 'pp'
 
 class Recipe
 
-  FOOD2FORK_KEY = "e8371d9ef1bd86f0a957b91684b5ec2a"
+  #FOOD2FORK_KEY = "e8371d9ef1bd86f0a957b91684b5ec2a"
   include HTTParty
 
-  base_uri 'http://food2fork.com/api/search'
-  default_params fields: "key, q:search"
+  base_uri 'http://food2fork.com/api/'
+  default_params key: ENV["FOOD2FORK_KEY"]
   format :json
 
-  def self.for term
-    get("", query: {query: term})["elements"]
+  def self.for keyword
+    get("/search", query: {q: keyword})["recipes"]
   end
 end
 
-pp Recipe.for "chicken"
+# p ENV["FOOD2FORK_KEY"]
+# pp Recipe.for "chicken"
